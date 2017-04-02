229e36420db0b8c846c89713fab84f999241bad1
cheratt karim <karim.cheratt@yahoo.ca>
2017-04-01 23:53:03 -0400

    rename public/all to public/any + fix login button and login form

--- module/Vitrine/src/Vitrine/Model/Factory/ShopGatewayFactory.php
+++ module/Vitrine/src/Vitrine/Model/Factory/ShopGatewayFactory.php
@@ -19,7 +19,7 @@ class ShopGatewayFactory
         $resultPrototype = new ResultSet();
         $resultPrototype->setArrayObjectPrototype(new ShopEntity());
         
-        $tableGateway = new TableGateway(self::TAB_NAME, $dbAdapter, null, $resultPrototype);
+        $tableGateway = new TableGateway(self::$TAB_NAME, $dbAdapter, null, $resultPrototype);
         return new ShopGateway($tableGateway);
     }
 }
